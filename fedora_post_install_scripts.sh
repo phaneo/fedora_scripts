00516542041e12a68bc8fff27ed7a93db2faf7aa
phaneo <phaneo.kaushik@gmail.com>
2022-11-15 21:47:56 +0530

    first commit

---
+++ fedora_post_install_scripts.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+echo -e "fastestmirror=True\ndeltarpm=True\nmax_parallel_downloads=10" | sudo tee -a /etc/dnf/dnf.conf
+sudo dnf update
+sudo hostnamectl set-hostname Phani-HP15
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo dnf config-manager --add-repo https://packages.microsoft.com/yumrepos/edge sudo mv /etc/yum.repos.d/packages.microsoft.com_yumrepos_edge.repo /etc/yum.repos.d/microsoft-edge-stable.repo
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+sudo dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo dnf install -y vlc gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel lame\* --exclude=lame-devel
+sudo dnf group upgrade --with-optional Multimedia
+sudo dnf in -y evolution gnome-tweaks gnome-extensions-app discord sublime-text microsoft-edge-stable 
+sudo dnf in -y GConf2
+sudo dnf in -y openssl
+sudo dnf copr enable jstaf/onedriver
+sudo dnf in -y onedriver
+sudo dnf in -y qbittorrent
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager     --add-repo     https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo usermod -aG docker $(whoami)
+newgrp docker
+sudo systemctl enable docker
+sudo systemctl start --now docker
+flatpak install slack
+flatpak install spotify
+
+
+
